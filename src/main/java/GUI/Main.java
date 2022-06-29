1d1c332f6f359512b4cee5e3ea7d09fa48536fe6
Ermolaefff <75524250+Ermolaefff@users.noreply.github.com>
2022-06-30 04:12:13 +0700

    Update Main.java

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,14 +1,12 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
 
 import java.io.IOException;
 
@@ -27,4 +25,3 @@ public class Main extends Application {
         primaryStage.show();
     }
 }
-
